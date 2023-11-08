4e0bed405f18ff1e11d5b496506353c5059593bd
syed <syedabrar56345@gmail.com>
2023-11-07 23:20:08 -0500

    Implemented custom string message publisher, style guide compliance, and linting/checking.

---
+++ src/subscriber.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 Abraruddin Syed
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+/**
+ * @brief A minimal ROS2 subscriber node.
+ */
+class MinimalSubscriber : public rclcpp::Node {
+ public:
+    /**
+     * @brief Constructor for the MinimalSubscriber class.
+     */
+    MinimalSubscriber() : Node("minimal_subscriber") {
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "topic", 10,
+            std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
+
+ private:
+    /**
+     * @brief Callback function for receiving messages.
+     *
+     * @param msg The received message.
+     */
+    void topic_callback(const std_msgs::msg::String& msg) const {
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+    }
+
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+/**
+ * @brief The main function to create and run the MinimalSubscriber node.
+ */
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::shutdown();
+    return 0;
+}
